e992610a89acc554fd62ef3f5317659127d937ca
Milena Andrada <raissamilena002@gmail.com>
2026-04-10 12:43:47 -0300

    feat: :sparkles: trabalho
    fizemos mais uma parte do trabalho

--- pokedex/app/welcome/welcome.tsx
+++ pokedex/app/welcome/welcome.tsx
@@ -9,42 +9,49 @@ export function Welcome() {
         <h1 className="align-text-top">Pokédex</h1>
         <h1 className="justify-self-end align-text-top">Home Pokémons Tipos</h1>
         
-        </div>
+      </div> 
+
+      <div className=" flex flex-col justify-between  bg-red ">
+      
         <div>
           <input
             
               type="button "
               value="busca "
               disabled
-              className="invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500 disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 disabled:shadow-none dark:disabled:border-gray-700 dark:disabled:bg-gray-800/20 ..."/>
+              className=" bg-white text-black placeholder:text-gray-500 border-gray-300 rounded-1g p-2 m-1 w-370 "/>
         </div>
-      <div className="flex justify-between p-3 ">
+       <div className="flex justify-between p-3 ">
         <div>
-          <button className="bg-yellow-400   box-border size-32 border-solid w-60 py-20">
+          <button className="bg-yellow-400   box-border size-32 border-solid w-60 py-20 rounded-lg">
             <h1>Pikachu</h1>
             <button className="bg-orange-600">Elétrico</button>
           </button>
         </div>
         <div>
-          <button className="bg-green-500 box-border size-32 border-solid w-60 py-20">
+          <button className="bg-green-500 box-border size-32 border-solid w-60 py-20 rounded-lg">
             <h1>Bulbasaur</h1>
             <button className="bg-green-800">Planta</button>
           </button>
         </div>
         <div>
-          <button className="bg-pink-400 box-border size-32 border-solid w-60 py-20">
+          <button className="bg-pink-400 box-border size-32 border-solid w-60 py-20 rounded-lg">
             <h1>Charmander</h1>
             <button className="bg-red-600">fogo</button>
           </button>
         </div>
         <div>
-          <button className="bg-blue-500 box-border size-32 border-solid w-60 py-20">
+          <button className="bg-blue-500 box-border size-32 border-solid w-60 py-20 rounded-lg">
             <h1>Squirtie</h1>
             <button className="bg-blue-700">Água</button>
           </button>
           </div>
-      </div> 
+        </div> 
       
+      <div className="bg-blue-900 justify-center">
+        <h1 className="justify-center align-text-top bg-white">Pokédex - React + Tailwind</h1>
+      </div>
+      </div>
     </main>
   );
 }
